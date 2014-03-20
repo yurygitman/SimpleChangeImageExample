a308cee3c47898bc31e3335772642c7e16047ff8
Yury G <yury@pulsesensor.com>
2014-03-20 14:37:06 -0400

    Initial Commit

---
+++ SimpleChangeImageExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleChangeImageExample
+//
+//  Created by GrownYoda on 3/20/14.
+//  Copyright (c) 2014 YuryGitman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleChangeImageExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleChangeImageExample
+//
+//  Created by GrownYoda on 3/20/14.
+//  Copyright (c) 2014 YuryGitman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
