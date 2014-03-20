3f4fc68ee53c1dee67e6aaac9c79b20797fef2ec
Yury G <yury@pulsesensor.com>
2014-03-20 14:47:10 -0400

    init commit

--- SimpleChangeImageExample/ViewController.h
+++ SimpleChangeImageExample/ViewController.h
@@ -2,12 +2,20 @@
 //  ViewController.h
 //  SimpleChangeImageExample
 //
-//  Created by GrownYoda on 3/20/14.
 //  Copyright (c) 2014 YuryGitman. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int counter;
+}
+
+//button
+- (IBAction)buttonPressed:(id)sender;
+
+// image object
+@property (strong, nonatomic) IBOutlet UIImageView *myImage;
 
 @end
--- SimpleChangeImageExample/ViewController.m
+++ SimpleChangeImageExample/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 YuryGitman. All rights reserved.
 //
 
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,7 +18,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+ //   counter = 1;
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //    [self displayImageWithName:@"1.png"];
+    
+}
+
+- (void) displayImageWithName: (NSString*)name{
+    
+   
+    
+    if (![name isEqualToString:@"remove"]) {
+        _myImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:name]];
+        [_myImage setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        [self.view addSubview:_myImage];
+    
+        NSLog(@"not remove");
+        
+    }
+    
+    if ([name isEqualToString:@"remove"]) {
+        
+        _myImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:name]];
+        [_myImage setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        _myImage = nil;
+        [_myImage removeFromSuperview];
+
+        NSLog(@"remove");
+
+    }
+    
+    // [self.view addSubview:_myImage];
+    NSLog(@"counter: %i",counter);
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +60,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    
+    counter ++;
+    
+    if (counter == 1) {
+        [self displayImageWithName:@"1.png"];
+    }
+    if (counter == 2) {
+        [self displayImageWithName:@"2.png"];
+    }
+    if (counter == 3) {
+        [self displayImageWithName:@"3.png"];
+    }
+    if (counter == 4) {
+        [self displayImageWithName:@"4.png"];
+        
+    }
+    if (counter > 4) {
+        [self displayImageWithName:@"remove"];
+        
+        counter = 0;
+    }
+    
+    
+    
+}
 @end
